1328e60ff1b307e64e86096ffd822c67c22a3e3e
Nodematic Labs <community@nodematic.com>
2023-07-13 22:22:54 -0400

    Create chrome.sh

---
+++ chrome.sh
@@ -0,0 +1,5 @@
+sudo apt install curl
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install google-chrome-stable
